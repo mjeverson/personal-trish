1acba7be0a16eb1840df7c26bd6c488181a7b1d1
Mike Everson <mike.everson@climaxmedia.com>
2014-06-24 14:02:13 -0400

    set up static headers/footers and parallax

--- _Global/js/main.js
+++ _Global/js/main.js
@@ -1,3 +1,74 @@
 $(document).ready(function(){
-	console.log('Hello world!');
+
+  //save selectors as variables to increase performance
+  var $window = $(window);
+  var $firstBG = $('#dodge');
+  var $secondBG = $('#ride-planner');
+  var $thirdBG = $('#rushmore');
+
+  var windowHeight = $window.height(); //get the height of the window
+
+  //apply the class "inview" to a section that is in the viewport
+  $('#dodge, #ride-planner, #rushmore').bind('inview', function (event, visible) {
+    if (visible == true) {
+      $(this).addClass("inview");
+    } else {
+      $(this).removeClass("inview");
+    }
+  });
+
+  //function that places the navigation in the center of the window
+  function RepositionNav(){
+    var windowHeight = $window.height(); //get the height of the window
+    var navHeight = $('#nav').height() / 2;
+    var windowCenter = (windowHeight / 2);
+    var newtop = windowCenter - navHeight;
+    $('#nav').css({"top": newtop}); //set the new top position of the navigation list
+  }
+
+  //function that is called for every pixel the user scrolls. Determines the position of the background
+  /*arguments:
+   x = horizontal position of background
+   windowHeight = height of the viewport
+   pos = position of the scrollbar
+   adjuster = adjust the position of the background
+   inertia = how fast the background moves in relation to scrolling
+   */
+  function newPos(x, windowHeight, pos, adjuster, inertia){
+    return x + "% " + (-((windowHeight + pos) - adjuster) * inertia)  + "px";
+  }
+
+  //function to be called whenever the window is scrolled or resized
+  function Move(){
+    var pos = $window.scrollTop(); //position of the scrollbar
+
+    //if the first section is in view...
+    if($firstBG.hasClass("inview")){
+      //call the newPos function and change the background position
+      $firstBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 900, 0.3)});
+    }
+
+    //if the second section is in view...
+    if($secondBG.hasClass("inview")){
+      //call the newPos function and change the background position
+      $secondBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 1250, 0.3)});
+    }
+
+    //if the third section is in view...
+    if($thirdBG.hasClass("inview")){
+      //call the newPos function and change the background position
+      $thirdBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 2850, 0.3)});
+    }
+  }
+
+  RepositionNav(); //Reposition the Navigation to center it in the window when the script loads
+
+  $window.resize(function(){ //if the user resizes the window...
+    Move(); //move the background images in relation to the movement of the scrollbar
+    RepositionNav(); //reposition the navigation list so it remains vertically central
+  });
+
+  $window.bind('scroll', function(){ //when the user is scrolling...
+    Move(); //move the background images in relation to the movement of the scrollbar
+  });
 });
