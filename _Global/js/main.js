2355352ac3143edc7d5e7dc82ccff10be79f3ecb
Mike Everson <mike.everson@climaxmedia.com>
2014-06-26 10:05:01 -0400

    hooked up header scrolling and update

--- _Global/js/main.js
+++ _Global/js/main.js
@@ -9,10 +9,28 @@ $(document).ready(function(){
 
   // Set up the nav
   var navInit = function(){
+    // Click to scroll
     $('.nav-container .nav').bind('click', function(){
+      $('.nav-container .nav').removeClass('hover');
+      $(this).addClass('hover');
+
       var pos = $($(this).attr('id')).offset().top;
       $("html, body").animate({ scrollTop: pos + "px" });
     });
+
+    /* Update headers while scrolling */
+    $('#about, #work, #contact').waypoint(function(d){
+      $('.nav-container .nav').removeClass('hover');
+      var id = $(this).attr('id');
+      var order = parseInt($(this).attr('order')) > 1 ? parseInt($(this).attr('order')) - 1 : parseInt($(this).attr('order'));
+
+      if(d == 'down') {
+        $('.nav-container .nav[id="#' + id + '"]').addClass('hover');
+      }
+      else if (d == 'up'){
+        $('.nav-container .nav[order="' + order + '"]').addClass('hover');
+      }
+    }, { offset: 117 });
   };
 
   // Initialize hover events
@@ -85,13 +103,12 @@ $(document).ready(function(){
   // Initialize the parallax styles and events
   var parallaxInit = function(){
     //save selectors as variables to increase performance
-    var $window = $(window);
     var $innerWrapperBG = $('.inner-wrapper');
     var $firstBG = $('#rushmore');
     var $secondBG = $('#dodge');
     var $thirdBG = $('#ride-planner');
 
-    var windowHeight = $window.height(); //get the height of the window
+    var windowHeight = $(window).height(); //get the height of the window
 
     //apply the class "inview" to a section that is in the viewport
     $('#dodge, #ride-planner, #rushmore').bind('inview', function (event, visible) {
@@ -110,22 +127,22 @@ $(document).ready(function(){
      adjuster = adjust the position of the background
      inertia = how fast the background moves in relation to scrolling
      */
-    function newPos(x, windowHeight, pos, adjuster, inertia){
+    var newPos = function (x, windowHeight, pos, adjuster, inertia){
       return x + "% " + (-((windowHeight + pos) - adjuster) * inertia)  + "px";
-    }
+    };
 
     //function to be called whenever the window is scrolled or resized
     //move the background images in relation to the movement of the scrollbar
-    function Move(){
-      var pos = $window.scrollTop(); //position of the scrollbar
+    var move = function(){
+      var pos = $(window).scrollTop(); //position of the scrollbar
 
-      // Adjust the inner wrapper position
+      // Adjust the inner wrapper position for main "Trish" BG
       $innerWrapperBG.css('background-position', 'left ' + ((pos)) + 'px');
 
       //if the first section is in view...
       if($firstBG.hasClass("inview")){
         //call the newPos function and change the background position
-        $firstBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 900, 0.1)});
+        $firstBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 900, 0.05)});
       }
 
       //if the second section is in view...
@@ -137,18 +154,18 @@ $(document).ready(function(){
       //if the third section is in view...
       if($thirdBG.hasClass("inview")){
         //call the newPos function and change the background position
-        $thirdBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 2850, 0.1)});
+        $thirdBG.css({'backgroundPosition': newPos(50, windowHeight, pos, 2850, 0.05)});
       }
-    }
+    };
 
     //if the user resizes the window...
-    $window.resize(function(){
-      Move();
+    $(window).resize(function(){
+      move();
     });
 
     //when the user is scrolling...
-    $window.bind('scroll', function(){
-      Move();
+    $(window).bind('scroll', function(){
+      move();
     });
 
     // click events for the parallax sections
